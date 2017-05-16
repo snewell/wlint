b585c6dd2bbe18504197e720f110603b12a1b258
Stephen Newell <sjnewell@gmail.com>
2017-05-16 09:17:07 -0600

    Use argparse instead of handrolling argument parsing

--- libexec/writing-tools/list-filter.py
+++ libexec/writing-tools/list-filter.py
@@ -1,31 +1,11 @@
 #!/usr/bin/python3
 
+import argparse
 import re
 from os import listdir
 from os import path
 from sys import argv
 from sys import stderr
-
-def printHelp(name, defaultLists):
-	name = path.basename(name)
-	print("{} - Detect troublesome words".format(name))
-	print("{} [options] <file1> [<file2> ...]".format(name))
-	print("\nOPTIONS")
-	print("  --help, -h       Display this help message and exit")
-	print("  --lists=<lists>  Change the set of word lists.  <lists> should be a")
-	print("                   comma-separated list of *builit-in* lists to use.")
-	print("                   [Default: {}]".format(", ".join(str(l) for l in defaultLists)))
-	print("  --list=<list>    Use a custom word list.  <list> should be a path to a text")
-	print("                   file with one word per line.")
-	print("  --               Stop processing options (required if files to parse match")
-	print("                   any of these options)")
-	print("\nEXAMPLES")
-	print("  {} foo.txt".format(name))
-	print("  {} foo.txt bar.txt".format(name))
-	print("  {} --lists=filter,weasel foo.txt".format(name))
-	print("  {} --list=my_list.txt foo.txt".format(name))
-	print("  {} --list=my_list.txt --list=another_list.txt foo.txt".format(name))
-	print("  {} --lists=filter --list=mylist.txt foo.txt".format(name))
 
 def addWords(path, words):
 	with open(path, "r") as inputList:
@@ -57,62 +37,50 @@ def readDefaults():
 		if m:
 			ret.append(m.group(1))
 	ret.sort()
-	return ret
+	return ",".join(ret)
 
-# default word lists
+def parseFiles(files, words, missing):
+	for f in files:
+		try:
+			with open(f, 'r') as readFile:
+					lineNumber = 0
+					for line in readFile:
+						lineNumber += 1
+						for word, pattern in words.items():
+							match = pattern.search(line)
+							while match:
+								print("{} {} ({}:{})".format(f, word, lineNumber, match.start()))
+								match = pattern.search(line, match.end())
+		except FileNotFoundError:
+			missing.append(f)
+
 defaultWordLists = readDefaults()
 
-if len(argv) > 1:
-	wordLists = defaultWordLists
-	extraLists = [ ]
+parser = argparse.ArgumentParser(description="Detect troublesome words")
+parser.add_argument("--lists", help="Change the set of word lists.  This should be a comma-separated list of built-in lists.  [Default={}]".format(defaultWordLists),
+                    default=defaultWordLists)
+parser.add_argument("--list", help="Use a custom word list.  The list should be a plain text file with one word per line.",
+                    action='append', default=[])
+parser.add_argument("--file", help="Process a file.  This is only necessary if an input file matches an argument (e.g., --help).",
+                    action='append', default=[])
+parser.add_argument("files", help="Files to process.", nargs='*', metavar="file")
 
-	index = 1
-	# parse optoins
-	while index < len(argv):
-		if argv[index] == "--help":
-			printHelp(argv[0], defaultWordLists)
-			exit(0)
-		elif argv[index] == "-h":
-			printHelp(argv[0], defaultWordLists)
-			exit(0)
-		elif argv[index] == "--":
-			break
-		elif re.match("^--lists=", argv[index]):
-			newList = argv[index].split('=')[1]
-			if len(newList) > 0:
-				wordLists = newList.split(",")
-			else:
-				wordLists = []
-		elif re.match("^--list=", argv[index]):
-			extraLists.append(argv[index].split('=')[1])
-		else:
-			# not an option
-			break
-		index += 1
+args = parser.parse_args()
+if len(args.files) > 0 or len(args.file) > 0:
+	args.lists = args.lists.split(",")
 
-	words = parseWordLists(wordLists)
-	for wordList in extraLists:
+	words = parseWordLists(args.lists)
+	for wordList in args.list:
 		addWords(wordList, words)
 
 	missingFiles = [ ]
-	# We should only have files at this point
-	while index < len(argv):
-		try:
-			with open(argv[index], 'r') as readFile:
-				lineNumber = 0
-				for line in readFile:
-					lineNumber += 1
-					for word, pattern in words.items():
-						match = pattern.search(line)
-						while match:
-							print("{} {} ({}:{})".format(argv[index], word, lineNumber, match.start()))
-							match = pattern.search(line, match.end())
-		except FileNotFoundError:
-			missingFiles.append(argv[index])
-		index += 1
+	parseFiles(args.files, words, missingFiles)
+	parseFiles(args.file, words, missingFiles)
 
 	if len(missingFiles) > 0:
 		print("Error opening files: {}".format(missingFiles), file=stderr)
 		exit(1)
+
 else:
-	printHelp(argv[0], defaultWordLists)
+	print("Error: no files specified ({} -h)".format(argv[0]), file=stderr)
+	exit(1)
