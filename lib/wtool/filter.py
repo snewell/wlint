25ba8a2ec2589195cd7913c620279130d08b4d07
Stephen Newell <sjnewell@gmail.com>
2017-05-19 09:26:19 -0600

    Switch tabs back to spaces (should have read PEP-8 before the last switch)

--- lib/wtool/filter.py
+++ lib/wtool/filter.py
@@ -4,49 +4,49 @@ import argparse
 import re
 
 class WordList:
-	"""A list of words to search for"""
-	def __init__(self):
-		self.words = { }
+    """A list of words to search for"""
+    def __init__(self):
+        self.words = { }
 
-	def addWord(self, word):
-		"""Add a word to the list.
+    def addWord(self, word):
+        """Add a word to the list.
 
-		Arguments:
-		word -- the word to add"""
-		pattern = re.compile("\\b{}\\b".format(word), re.IGNORECASE)
-		self.words[word] = pattern
+        Arguments:
+        word -- the word to add"""
+        pattern = re.compile("\\b{}\\b".format(word), re.IGNORECASE)
+        self.words[word] = pattern
 
-	def addWords(self, path):
-		"""Add words from a file.  Each entry should be on a new line and is
-		treated as the literal entry.
+    def addWords(self, path):
+        """Add words from a file.  Each entry should be on a new line and is
+        treated as the literal entry.
 
-		Arguments:
-		path -- path of the file to parse"""
-		with open(path, "r") as inputList:
-			for line in inputList:
-				self.addWord(line[:-1])
+        Arguments:
+        path -- path of the file to parse"""
+        with open(path, "r") as inputList:
+            for line in inputList:
+                self.addWord(line[:-1])
 
 class Filter:
-	"""An object to filter files."""
-	def __init__(self, words):
-		"""Construct a Filter.
+    """An object to filter files."""
+    def __init__(self, words):
+        """Construct a Filter.
 
-		Arguments:
-		words -- the WordList to use"""
-		self.words = words
+        Arguments:
+        words -- the WordList to use"""
+        self.words = words
 
-	def parseFile(self, path, fn):
-		"""Parse a file.
+    def parseFile(self, path, fn):
+        """Parse a file.
 
-		Arguments:
-		path -- the file to parse
-		fn -- A function to invoke on each match.  Arguments are: word, lineNumber, column."""
-		with open(path, 'r') as readFile:
-			lineNumber = 0
-			for line in readFile:
-				lineNumber += 1
-				for word, pattern in self.words.words.items():
-					match = pattern.search(line)
-					while match:
-						fn(word, lineNumber, match.start())
-						match = pattern.search(line, match.end())
+        Arguments:
+        path -- the file to parse
+        fn -- A function to invoke on each match.  Arguments are: word, lineNumber, column."""
+        with open(path, 'r') as readFile:
+            lineNumber = 0
+            for line in readFile:
+                lineNumber += 1
+                for word, pattern in self.words.words.items():
+                    match = pattern.search(line)
+                    while match:
+                        fn(word, lineNumber, match.start())
+                        match = pattern.search(line, match.end())
--- libexec/writing-tools/list-filter.py
+++ libexec/writing-tools/list-filter.py
@@ -9,51 +9,51 @@ from sys import argv
 from sys import stderr
 
 def parseWordLists(path, wordLists):
-	"""Parse built-in word lists for filtering.
+    """Parse built-in word lists for filtering.
 
-	Arguments:
-	path -- the path where built-in strings are stored
-	wordLists -- the files to read from path"""
-	words = filter.WordList()
+    Arguments:
+    path -- the path where built-in strings are stored
+    wordLists -- the files to read from path"""
+    words = filter.WordList()
 
-	for wordList in wordLists:
-		filePath = "{}/{}-words.txt".format(path, wordList)
-		try:
-			words.addWords(filePath)
-		except FileNotFoundError:
-			# Assuming no built in lists is the exception, not the rule.
-			if wordList != "":
-				print("Invalid list: {}".format(wordList), file=stderr)
-	return words
+    for wordList in wordLists:
+        filePath = "{}/{}-words.txt".format(path, wordList)
+        try:
+            words.addWords(filePath)
+        except FileNotFoundError:
+            # Assuming no built in lists is the exception, not the rule.
+            if wordList != "":
+                print("Invalid list: {}".format(wordList), file=stderr)
+    return words
 
 def readDefaults(path):
-	"""Get a list of built-in word lists.
+    """Get a list of built-in word lists.
 
-	Arguments:
-	path -- the path where built-in strings are stored"""
-	files = listdir(path)
-	pattern = re.compile("^([a-z]+)-words.txt$")
-	ret = [ ]
-	for file in files:
-		m = pattern.search(file)
-		if m:
-			ret.append(m.group(1))
-	ret.sort()
-	return ",".join(ret)
+    Arguments:
+    path -- the path where built-in strings are stored"""
+    files = listdir(path)
+    pattern = re.compile("^([a-z]+)-words.txt$")
+    ret = [ ]
+    for file in files:
+        m = pattern.search(file)
+        if m:
+            ret.append(m.group(1))
+    ret.sort()
+    return ",".join(ret)
 
 def parseFiles(files, filter, hits, missing):
-	"""Parse a list of files, searching for filtered words.
+    """Parse a list of files, searching for filtered words.
 
-	Arguments:
-	files -- a list of files to parse (each file should be a full path)
-	filter -- a Filter object
-	hits -- a list to store filter's matches
-	missing -- a list to store entires in files that don't exist"""
-	for f in files:
-		try:
-			filter.parseFile(f, lambda word, line, col: hits.append((f, word, line, col)))
-		except FileNotFoundError:
-			missing.append(f)
+    Arguments:
+    files -- a list of files to parse (each file should be a full path)
+    filter -- a Filter object
+    hits -- a list to store filter's matches
+    missing -- a list to store entires in files that don't exist"""
+    for f in files:
+        try:
+            filter.parseFile(f, lambda word, line, col: hits.append((f, word, line, col)))
+        except FileNotFoundError:
+            missing.append(f)
 
 listDir = "{}/../../share/writing-tools/filter-lists".format(path.abspath(path.dirname(argv[0])))
 defaultWordLists = readDefaults(listDir)
@@ -69,28 +69,28 @@ parser.add_argument("files", help="Files to process.", nargs='*', metavar="file"
 
 args = parser.parse_args()
 if len(args.files) > 0 or len(args.file) > 0:
-	args.lists = args.lists.split(",")
+    args.lists = args.lists.split(",")
 
-	words = parseWordLists(listDir, args.lists)
-	for wordList in args.list:
-		try:
-			words.addWords(wordList, words)
-		except FileNotFoundError:
-			print("Invalid list: {}".format(wordList), file=stderr)
+    words = parseWordLists(listDir, args.lists)
+    for wordList in args.list:
+        try:
+            words.addWords(wordList, words)
+        except FileNotFoundError:
+            print("Invalid list: {}".format(wordList), file=stderr)
 
-	filter = filter.Filter(words)
-	hits = [ ]
-	missingFiles = [ ]
-	parseFiles(args.files, filter, hits, missingFiles)
-	parseFiles(args.file, filter, hits, missingFiles)
+    filter = filter.Filter(words)
+    hits = [ ]
+    missingFiles = [ ]
+    parseFiles(args.files, filter, hits, missingFiles)
+    parseFiles(args.file, filter, hits, missingFiles)
 
-	for (file, word, line, col) in hits:
-		 print("{} {} ({}:{})".format(file, word, line, col))
+    for (file, word, line, col) in hits:
+        print("{} {} ({}:{})".format(file, word, line, col))
 
-	if len(missingFiles) > 0:
-		print("Error opening files: {}".format(missingFiles), file=stderr)
-		exit(1)
+    if len(missingFiles) > 0:
+        print("Error opening files: {}".format(missingFiles), file=stderr)
+        exit(1)
 
 else:
-	print("Error: no files specified ({} -h)".format(argv[0]), file=stderr)
-	exit(1)
+    print("Error: no files specified ({} -h)".format(argv[0]), file=stderr)
+    exit(1)
