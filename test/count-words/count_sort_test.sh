927e7506341dd77f5477541e13e0e3c1841836dd
Stephen Newell <sjnewell@gmail.com>
2017-07-05 22:00:59 -0600

    Start punctuation-rules tests

--- test/count-words/count_sort_test.sh
+++ test/count-words/count_sort_test.sh
@@ -4,6 +4,6 @@ current_dir=$(dirname ${0})
 current_script=$(basename ${0})
 
 input="${current_dir}/${current_script}.in"
-python ${WLINT_TOOL_DIR}/count-words.py --sort-count ${input} |
+python3 ${WLINT_TOOL_DIR}/count-words.py --sort-count ${input} |
 	tail -n +2 |
 	diff "${current_dir}/${current_script}.out" -
