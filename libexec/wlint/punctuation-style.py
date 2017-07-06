695304372ebe4ad76c5d6ce7dea7f39c28ffba07
Stephen Newell <sjnewell@gmail.com>
2017-07-05 22:27:01 -0600

    Sort punctuation hits so output is based on line and column, not the order rules are checked

--- libexec/wlint/punctuation-style.py
+++ libexec/wlint/punctuation-style.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import re
+import operator
 
 import wlint.common
 import wlint.punctuation
@@ -17,13 +17,16 @@ class PunctuationStyle(wlint.common.Tool):
 
     def process(self, fileHandle):
         lineNumber = 0
+        hits = []
         for text in fileHandle:
             lineNumber += 1
             for message, fn in self.checks.items():
-                if fn(text, lambda pos: print(
-                    "{}-{}:{} {}".format(fileHandle.name, lineNumber,
-                                         pos, message))):
+                if fn(text, lambda pos: hits.append(lineNumber, pos, message)):
                     self.result = 1
+
+        hits.sort()
+        for (line, col, message) in hits:
+            print("{}-{}:{} {}".format(fileHandle.name, line, pos, message))
 
 
 punctuationStyle = PunctuationStyle("Check for common punctuation issues")
