c3879325079ea5282539d41c559de7f571a2d288
Stephen Newell <sjnewell@gmail.com>
2017-10-21 22:43:55 -0600

    Improve punctuation testing

--- lib/test/test_quote_rules.py
+++ lib/test/test_quote_rules.py
@@ -7,11 +7,17 @@ import env
 import wlint.punctuation
 
 def _run_rules(ut, text, expected_rule=None, expected_position=None):
+    hit = False
     def hit_fn(rule_message, pos):
+        nonlocal hit
+
         ut.assertEqual(rule_message, expected_rule)
         ut.assertEqual(pos, expected_position)
+        hit = True
 
     wlint.punctuation.check_rules(ut.rules, text, hit_fn)
+    if expected_position or expected_rule:
+        ut.assertTrue(hit)
 
 
 class TestQuotationRules(unittest.TestCase):
